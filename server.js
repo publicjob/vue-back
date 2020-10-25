60660b8e82a2e5baca2ddf83ee6ace083ad790b5
work.cjl <work.cjl@hotmail.com>
2020-10-25 16:19:34 +0800

    feat: init

---
+++ server.js
@@ -0,0 +1,24 @@
+const http = require('http')
+const users = [
+  {
+    id: 1,
+    name:'worker1'
+  },{
+    id: 2,
+    name: 'worker2'
+  },{
+    id: 3,
+    name: 'worker3'
+  }
+]
+
+const server = http.createServer(function(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  if(req.url === '/api/user') {
+    res.end(JSON.stringify(users))
+  }else{
+    res.end('not found')
+  }
+})
+
+server.listen(3000, () => console.log('server在3000端口监听中'))
